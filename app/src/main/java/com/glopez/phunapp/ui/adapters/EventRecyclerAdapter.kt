a4aab6c3c6a9665c801297d4404ea0132ff3bd63
George Lopez <george.ramirez.lopez@gmail.com>
2019-02-09 16:21:43 -0800

    Cleaned up spacing between event TextViews and ImageView.  Reduced event description to two lines.

--- app/src/main/java/com/glopez/phunapp/ui/adapters/EventRecyclerAdapter.kt
+++ app/src/main/java/com/glopez/phunapp/ui/adapters/EventRecyclerAdapter.kt
@@ -4,9 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.glopez.phunapp.R
 import android.widget.TextView
 import com.glopez.phunapp.data.Event
+import com.squareup.picasso.Picasso
 
 class EventRecyclerAdapter(private val eventList: List<Event>) :
         RecyclerView.Adapter<EventRecyclerAdapter.ViewHolder>() {
@@ -26,12 +28,22 @@ class EventRecyclerAdapter(private val eventList: List<Event>) :
 
         holder.eventTitle?.text = event.title
         holder.eventLocation?.text = event.location1
+        holder.eventDescription?.text = event.description
+        holder.eventImage
+        Picasso.get()
+            .load(event.image)
+            .placeholder(R.mipmap.ic_launcher_round)
+            .error(R.mipmap.ic_launcher_round)
+            .resize(72, 72)
+            .centerCrop()
+            .into(holder.eventImage)
     }
-
 
     class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
         val eventTitle = itemView?.findViewById<TextView>(R.id.event_title)
         val eventLocation = itemView?.findViewById<TextView>(R.id.event_location)
+        val eventDescription = itemView?.findViewById<TextView>(R.id.event_description)
+        val eventImage = itemView?.findViewById<ImageView>(R.id.event_image)
     }
 
 }
