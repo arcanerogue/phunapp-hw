edfe00abce168685322f5c1539276eb26527c9b1
George Lopez <george.ramirez.lopez@gmail.com>
2019-03-17 03:35:14 -0700

    Updated EventDetailActivity and activity_event_detail layout with collapsing toolbar containing event image and action icons.  Created landscape layout of activity_event_detail.

--- app/src/main/java/com/glopez/phunapp/ui/activities/EventDetailActivity.kt
+++ app/src/main/java/com/glopez/phunapp/ui/activities/EventDetailActivity.kt
@@ -2,14 +2,19 @@ package com.glopez.phunapp.ui.activities
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.glopez.phunapp.R
 import com.glopez.phunapp.ui.viewmodels.EventViewModel
+import kotlinx.android.synthetic.main.activity_event_detail.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,6 +24,13 @@ class EventDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_detail)
+        setSupportActionBar(detail_toolbar)
+
+        // Show the Up button in the action bar and
+        // hide the app name.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
 
         val eventImage: ImageView = findViewById(R.id.detail_event_image)
         val eventDate: TextView = findViewById(R.id.detail_event_date)
@@ -55,4 +67,28 @@ class EventDetailActivity : AppCompatActivity() {
         formatter.timeZone = TimeZone.getDefault()
         return formatter.format(this)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            android.R.id.home -> {
+                navigateUpTo(Intent(this, MainActivity::class.java))
+                true
+            }
+            R.id.detail_action_call -> {
+                Toast.makeText(this, "Place a call.", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.detail_action_share -> {
+                Toast.makeText(this, "Share event.", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
 }
