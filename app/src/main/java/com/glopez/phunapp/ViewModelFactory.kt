5afb51bc3dc384e21a8a8082bc57c74e446fcb8e
George Lopez <george.ramirez.lopez@gmail.com>
2019-05-18 13:04:15 -0700

    Removed EventDetailViewModel dependencies.  Added EventDetailViewModel creation in ViewModelFactory.

--- app/src/main/java/com/glopez/phunapp/ViewModelFactory.kt
+++ app/src/main/java/com/glopez/phunapp/ViewModelFactory.kt
@@ -3,17 +3,22 @@ package com.glopez.phunapp
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import com.glopez.phunapp.model.EventRepository
+import com.glopez.phunapp.view.viewmodels.EventDetailViewModel
 import com.glopez.phunapp.view.viewmodels.EventViewModel
 import java.lang.IllegalArgumentException
 
 class ViewModelFactory private constructor(private val eventRepository: EventRepository
 ) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(EventViewModel::class.java)) {
-            return EventViewModel(eventRepository) as T
-        } else {
-            throw IllegalArgumentException(
-                "Unknown ViewModel class provided: ${modelClass.name}")
+        return when {
+            modelClass.isAssignableFrom(EventViewModel::class.java) ->
+                EventViewModel(eventRepository) as T
+            modelClass.isAssignableFrom(EventDetailViewModel::class.java) ->
+                EventDetailViewModel(eventRepository) as T
+            else ->
+                throw IllegalArgumentException(
+                    "Unknown ViewModel class provided: ${modelClass.name}"
+                )
         }
     }
     companion object {
--- app/src/main/java/com/glopez/phunapp/view/viewmodels/EventDetailViewModel.kt
+++ app/src/main/java/com/glopez/phunapp/view/viewmodels/EventDetailViewModel.kt
@@ -3,15 +3,12 @@ package com.glopez.phunapp.view.viewmodels
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.ViewModel
 import com.glopez.phunapp.PhunApp
 import com.glopez.phunapp.model.Event
 import com.glopez.phunapp.model.EventRepository
 
-class EventDetailViewModel(application: Application) : AndroidViewModel(application)  {
-
-    private val phunApp = application as PhunApp
-    private val eventRepo: EventRepository = phunApp.getRepository()
-
+class EventDetailViewModel(private val eventRepo: EventRepository) : ViewModel() {
     fun getEvent(id: Int): LiveData<Event> {
         return eventRepo.getSingleEvent(id)
     }
